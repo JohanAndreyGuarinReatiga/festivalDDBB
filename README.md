392f0a8f5d7634f6a9c3124b795a632445fdd6e7
JohanAndreyGuarinReatiga <186079123+JohanAndreyGuarinReatiga@users.noreply.github.com.>
2025-06-18 16:10:50 -0500

    feat: :memo: Aggregation Framework part

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Consultas
 
 
-## Expresiones Regulares
+## 1 Expresiones Regulares
 Buscar bandas cuyo nombre empiece por la letra “A”.
 
 ![alt text](image.png)
@@ -9,3 +9,17 @@ Buscar bandas cuyo nombre empiece por la letra “A”.
 Buscar asistentes cuyo nombre contenga "Gómez".
 
 ![alt text](image-1.png)
+
+## 2 Operadores de Arreglos
+Buscar asistentes que tengan "Rock" dentro de su campo generos_favoritos.
+
+![alt text](image-2.png)
+
+## 3 Aggregation Framework
+Agrupar presentaciones por escenario y contar cuántas presentaciones hay por cada uno.
+
+![alt text](image-3.png)
+
+Calcular el promedio de duración de las presentaciones.
+
+![alt text](image-4.png)
--- consultas.js
+++ consultas.js
@@ -7,4 +7,35 @@
 
     // - Buscar asistentes cuyo **nombre contenga "Gómez"**. 
 
-    db.asistentes.find({ "nombre": { "$regex": "Gómez" } })     
+    db.asistentes.find({ "nombre": { "$regex": "Gómez" } })
+
+
+// 2. **Operadores de Arreglos**
+    // - Buscar asistentes que tengan `"Rock"` dentro de su campo `generos_favoritos`.
+
+    db.asistentes.find({generos_favoritos:{$in: ["Rock"]}})
+
+// 3. **Aggregation Framework**
+// - Agrupar presentaciones por `escenario` y contar cuántas presentaciones hay por cada uno.
+
+db.presentaciones.aggregate([{$group: {
+    _id: "$escenario",
+            total_presentaciones: { $sum: 1 }
+          },
+        },
+        {
+          $sort: { total_presentaciones: -1 },
+        },
+      ])
+        
+// - Calcular el **promedio de duración** de las presentaciones.
+db.presentaciones.aggregate([
+    {
+      $group: {
+        _id: null,
+        duracion_promedio: { $avg: "$duracion_minutos" },
+        duracion_total: { $sum: "$duracion_minutos" },
+        total_presentaciones: { $sum: 1 },
+      },
+    },
+  ])
